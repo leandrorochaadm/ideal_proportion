6f65f5e02e607bc71d85d08177b0ee6528ae0813
LeandroRocha <leandro@eulabs.com.br>
2022-01-12 11:56:40 -0400

    criação do projeto

---
+++ android/app/src/main/kotlin/br/com/leandroprogramador/ideal_proportion/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.leandroprogramador.ideal_proportion
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:ideal_proportion/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Proporção Ideal',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
